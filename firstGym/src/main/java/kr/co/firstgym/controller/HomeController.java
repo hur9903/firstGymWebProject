23ce48b763fa33aa8d508ade5d6dfe4c50297507
Deyeong Lee <woozu99@naver.com>
2021-09-17 14:34:55 +0900

    세팅 3차

--- firstGym/src/main/java/kr/co/firstgym/controller/HomeController.java
+++ firstGym/src/main/java/kr/co/firstgym/controller/HomeController.java
@@ -2,16 +2,14 @@ package kr.co.firstgym.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 	
 	// 홈 화면으로 이동
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String home() {
-		return "";
+		return "home/home";
 	}
 	
 }
